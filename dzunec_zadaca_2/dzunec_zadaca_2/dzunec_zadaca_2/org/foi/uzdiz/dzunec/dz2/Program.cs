3b70f9ee9dd06235c070ce4a47579d1173814746
Darko Žunec <darko.zunec@gmail.com>
2018-11-26 15:41:08 +0100

    Dodijeljen otpad korisnicima - odvojeno posebno!

--- dzunec_zadaca_2/dzunec_zadaca_2/dzunec_zadaca_2/org/foi/uzdiz/dzunec/dz2/Program.cs
+++ dzunec_zadaca_2/dzunec_zadaca_2/dzunec_zadaca_2/org/foi/uzdiz/dzunec/dz2/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2.Models;
 using org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2.Composite;
+using org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2.org.foi.uzdiz.dzunec.dz2.Helper;
 
 namespace org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2
 {
@@ -53,9 +54,9 @@ namespace org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2
 
             //Generiranje otpada korisnicima
 
-            DodijeliOtpadMalim(singletonParametri, genSlucajnihBrojeva);
-            DodijeliOtpadSrednjim(singletonParametri, genSlucajnihBrojeva);
-            DodijeliOtpadVelikim(singletonParametri, genSlucajnihBrojeva);
+            DodjelaOtpada.DodijeliOtpadMalim(singletonParametri, genSlucajnihBrojeva);
+            DodjelaOtpada.DodijeliOtpadSrednjim(singletonParametri, genSlucajnihBrojeva);
+            DodjelaOtpada.DodijeliOtpadVelikim(singletonParametri, genSlucajnihBrojeva);
 
             Console.WriteLine("--------------------------------------------");
             Console.WriteLine("++++++++++++++ MALI KORISNICI ++++++++++++++");
@@ -112,102 +113,6 @@ namespace org.foi.uzdiz.dzunec.dz2.dzunec_zadaca_2
             System.Console.ReadKey();
 
         }
-        public static void DodijeliOtpadMalim(SingletonParametri singletonParametri, SingletonGenSlucajnihBrojeva genSlucajnihBrojeva)
-        {
-            foreach (var ulica in Citac.ListaUlica)
-            {
-                foreach (var korisnik in ulica.ListaMalihKorisnika)
-                {
-                    double maliMinS = double.Parse(singletonParametri.DohvatiParametar("maliStaklo"));
-                    double maliMaxS = double.Parse(singletonParametri.DohvatiParametar("maliStaklo")) * double.Parse(singletonParametri.DohvatiParametar("maliMin")) / 100;
-                    korisnik.Staklo = genSlucajnihBrojeva.SlucajniBrojFloat(maliMinS, maliMaxS, 2);
-
-                    double maliMinP = double.Parse(singletonParametri.DohvatiParametar("maliPapir"));
-                    double maliMaxP = double.Parse(singletonParametri.DohvatiParametar("maliPapir")) * double.Parse(singletonParametri.DohvatiParametar("maliMin")) / 100;
-                    korisnik.Papir = genSlucajnihBrojeva.SlucajniBrojFloat(maliMinP, maliMaxP, 2);
-
-                    double maliMinM = double.Parse(singletonParametri.DohvatiParametar("maliMetal"));
-                    double maliMaxM = double.Parse(singletonParametri.DohvatiParametar("maliMetal")) * double.Parse(singletonParametri.DohvatiParametar("maliMin")) / 100;
-                    korisnik.Metal = genSlucajnihBrojeva.SlucajniBrojFloat(maliMinM, maliMaxM, 2);
-
-                    double maliMinB = double.Parse(singletonParametri.DohvatiParametar("maliBio"));
-                    double maliMaxB = double.Parse(singletonParametri.DohvatiParametar("maliBio")) * double.Parse(singletonParametri.DohvatiParametar("maliMin")) / 100;
-                    korisnik.Bio = genSlucajnihBrojeva.SlucajniBrojFloat(maliMinB, maliMaxB, 2);
-
-                    double maliMinMj = double.Parse(singletonParametri.DohvatiParametar("maliMješano"));
-                    double maliMaxMj = double.Parse(singletonParametri.DohvatiParametar("maliMješano")) * double.Parse(singletonParametri.DohvatiParametar("maliMin")) / 100;
-                    korisnik.Mjesano = genSlucajnihBrojeva.SlucajniBrojFloat(maliMinMj, maliMaxMj, 2);
-
-                }
-
-            }
-
-        }
-
-        public static void DodijeliOtpadSrednjim(SingletonParametri singletonParametri, SingletonGenSlucajnihBrojeva genSlucajnihBrojeva)
-        {
-            foreach (var ulica in Citac.ListaUlica)
-            {
-                foreach (var korisnik in ulica.ListaSrednjihKorisnika)
-                {
-                    double srednjiMinS = double.Parse(singletonParametri.DohvatiParametar("srednjiStaklo"));
-                    double srednjiMaxS = double.Parse(singletonParametri.DohvatiParametar("srednjiStaklo")) * double.Parse(singletonParametri.DohvatiParametar("srednjiMin")) / 100;
-                    korisnik.Staklo = genSlucajnihBrojeva.SlucajniBrojFloat(srednjiMinS, srednjiMaxS, 2);
-
-                    double srednjiMinP = double.Parse(singletonParametri.DohvatiParametar("srednjiPapir"));
-                    double srednjiMaxP = double.Parse(singletonParametri.DohvatiParametar("srednjiPapir")) * double.Parse(singletonParametri.DohvatiParametar("srednjiMin")) / 100;
-                    korisnik.Papir = genSlucajnihBrojeva.SlucajniBrojFloat(srednjiMinP, srednjiMaxP, 2);
-
-                    double srednjiMinM = double.Parse(singletonParametri.DohvatiParametar("srednjiMetal"));
-                    double srednjiMaxM = double.Parse(singletonParametri.DohvatiParametar("srednjiMetal")) * double.Parse(singletonParametri.DohvatiParametar("srednjiMin")) / 100;
-                    korisnik.Metal = genSlucajnihBrojeva.SlucajniBrojFloat(srednjiMinM, srednjiMaxM, 2);
-
-                    double srednjiMinB = double.Parse(singletonParametri.DohvatiParametar("srednjiBio"));
-                    double srednjiMaxB = double.Parse(singletonParametri.DohvatiParametar("srednjiBio")) * double.Parse(singletonParametri.DohvatiParametar("srednjiMin")) / 100;
-                    korisnik.Bio = genSlucajnihBrojeva.SlucajniBrojFloat(srednjiMinB, srednjiMaxB, 2);
-
-                    double srednjiMinMj = double.Parse(singletonParametri.DohvatiParametar("srednjiMješano"));
-                    double srednjiMaxMj = double.Parse(singletonParametri.DohvatiParametar("srednjiMješano")) * double.Parse(singletonParametri.DohvatiParametar("srednjiMin")) / 100;
-                    korisnik.Mjesano = genSlucajnihBrojeva.SlucajniBrojFloat(srednjiMinMj, srednjiMaxMj, 2);
-
-                }
-
-            }
-
-        }
-
-        public static void DodijeliOtpadVelikim(SingletonParametri singletonParametri, SingletonGenSlucajnihBrojeva genSlucajnihBrojeva)
-        {
-            foreach (var ulica in Citac.ListaUlica)
-            {
-                foreach (var korisnik in ulica.ListaVelikihKorisnika)
-                {
-                    double velikiMinS = double.Parse(singletonParametri.DohvatiParametar("velikiStaklo"));
-                    double velikiMaxS = double.Parse(singletonParametri.DohvatiParametar("velikiStaklo")) * double.Parse(singletonParametri.DohvatiParametar("velikiMin")) / 100;
-                    korisnik.Staklo = genSlucajnihBrojeva.SlucajniBrojFloat(velikiMinS, velikiMaxS, 2);
-
-                    double velikiMinP = double.Parse(singletonParametri.DohvatiParametar("velikiPapir"));
-                    double velikiMaxP = double.Parse(singletonParametri.DohvatiParametar("velikiPapir")) * double.Parse(singletonParametri.DohvatiParametar("velikiMin")) / 100;
-                    korisnik.Papir = genSlucajnihBrojeva.SlucajniBrojFloat(velikiMinP, velikiMaxP, 2);
-
-                    double velikiMinM = double.Parse(singletonParametri.DohvatiParametar("velikiMetal"));
-                    double velikiMaxM = double.Parse(singletonParametri.DohvatiParametar("velikiMetal")) * double.Parse(singletonParametri.DohvatiParametar("velikiMin")) / 100;
-                    korisnik.Metal = genSlucajnihBrojeva.SlucajniBrojFloat(velikiMinM, velikiMaxM, 2);
-
-                    double velikiMinB = double.Parse(singletonParametri.DohvatiParametar("velikiBio"));
-                    double velikiMaxB = double.Parse(singletonParametri.DohvatiParametar("velikiBio")) * double.Parse(singletonParametri.DohvatiParametar("velikiMin")) / 100;
-                    korisnik.Bio = genSlucajnihBrojeva.SlucajniBrojFloat(velikiMinB, velikiMaxB, 2);
-
-                    double velikiMinMj = double.Parse(singletonParametri.DohvatiParametar("velikiMješano"));
-                    double velikiMaxMj = double.Parse(singletonParametri.DohvatiParametar("velikiMješano")) * double.Parse(singletonParametri.DohvatiParametar("velikiMin")) / 100;
-                    korisnik.Mjesano = genSlucajnihBrojeva.SlucajniBrojFloat(velikiMinMj, velikiMaxMj, 2);
-
-                }
-
-            }
-
-        }
-
 
 
     }
